66c1d464da675407792497991ff6b55d29240488
Vadim Fedorovich <72505527+Gnarll@users.noreply.github.com>
2021-03-14 15:58:59 +0300

    feat: solve problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let tempStr;
+  while(str){
+    tempStr = str;
+    bracketsConfig.forEach(twoBrackets => str = str.replace(twoBrackets.join(""), ""));
+    if(tempStr == str) return false;
+  }
+
+  if(str == "") return true;
+  else return false;
+
 }
